084c5a2b30c0154987876929679bf28d1c5acd46
Carlos Villa <cvillad@uninorte.edu.co>
2021-05-01 12:34:02 -0500

    add number of boxes per account validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,9 +4,10 @@ class Account < ApplicationRecord
   validates :plan, presence: true
   validates :subdomain, presence: true, uniqueness: {case_sensitive: false}
 
-  has_many :members
+  has_many :members, dependent: :destroy
   has_many :users, through: :members
-  has_one :payment_method
+  has_many :boxes, dependent: :destroy
+  has_one :payment_method, dependent: :destroy
   belongs_to :user
 
   def subscribe(payment_method_params)
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,15 @@
 class Box < ApplicationRecord
   acts_as_tenant :account
+  validate :box_by_plan
+
+  def box_by_plan
+    if self.new_record? && account.plan != "unlimited"
+      n_boxes = account.boxes.count
+      if account.plan == "free" && n_boxes > 0
+        errors.add(:base, "Free plans cannot have more than one boxes")
+      elsif account.plan == "moderate" && n_boxes > 4
+        errors.add(:base, "Moderate plans cannot have more than five boxes")
+      end
+    end
+  end
 end
