f0f3f178e84b7a47407f067ec681f5f9180b4747
Carlos Villa <cvillad@uninorte.edu.co>
2021-05-01 16:05:30 -0500

    add some specs for registrations

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper' 
+
+RSpec.describe Users::RegistrationsController, type: :request do 
+  describe "#create" do 
+    shared_examples_for "user_creation" do 
+      it "should create a user" do 
+        expect{subject}.to change{User.count}.by(1)
+      end
+
+      it "should create a member" do 
+        expect{subject}.to change{Member.count}.by(1)
+      end
+
+      it "should create an account" do 
+        expect{subject}.to change{Account.count}.by(1)
+      end
+    end
+
+    subject {post "/users", params: params }
+    
+    context "when provided data is valid" do
+      describe "and plan is free" do 
+        let(:params) do 
+          {
+            user: {
+              email: "example@example.com",
+              password: "secret",
+              password_confirmation: "secret",
+              account_attributes: {
+                name: "example account",
+                subdomain: "example",
+                plan: "free"
+              }
+            }
+          }
+        end
+
+        it_behaves_like "user_creation"
+      end
+
+      describe "and plan is not free" do 
+        let(:params) do 
+          {
+            user: {
+              email: "example@example.com",
+              password: "secret",
+              password_confirmation: "secret",
+              account_attributes: {
+                name: "example account",
+                subdomain: "example",
+                plan: "moderate"
+              }
+            },
+            payment_method: {
+              number: "4242424242424242",
+              cvc: "145",
+              exp_month: "12",
+              exp_year: "2025"
+            }
+          }
+        end
+        it_behaves_like "user_creation"
+
+        it "should create a payment method" do 
+          expect{subject}.to change{PaymentMethod.count}.by(1)
+        end
+      end
+    end
+  end
+end
