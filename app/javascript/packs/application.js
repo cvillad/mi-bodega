ed567461e2f65437ed1cf02ee28a62c77ca9673d
Carlos Villa <cvillad@uninorte.edu.co>
2021-05-01 11:52:57 -0500

    hide payment information for free plan

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,22 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on("turbolinks:load", function() {
+  const handlePlanChange = function(plan_type) {
+    if(plan_type == undefined) {
+      plan_type = $('#tenant_plan :selected').val();
+    }
+    if( plan_type === 'moderate' || plan_type === 'unlimited') {
+      $('[data-stripe]').show();
+    } else {
+      $('[data-stripe]').hide();
+    }
+  }
+  
+  handlePlanChange($('#tenant_plan :selected').val())
+  // Set up plan change event listener #tenant_plan id in the forms for class cc_form
+  $("#tenant_plan").on('change', function(event) {
+    handlePlanChange($('#tenant_plan :selected').val());
+  });
+});
