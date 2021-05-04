f0e77435924df9a3920114f1402dc995208608e0
Carlos Villa <cvillad@uninorte.edu.co>
2021-05-04 14:33:37 -0500

    modify home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,8 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!
 
   def index
+    if current_user 
+      redirect_to accounts_path, alert: "You are signed in."
+    end
   end
 end
